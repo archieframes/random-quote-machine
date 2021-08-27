41a8d514521c67ac1009d637789bfd297e89ce98
Austin Reeves <contact.austinreeves@gmail.com>
2021-08-27 11:34:09 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import quotes from './quotes.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            index: undefined,
+            quote: "",
+            author: ""
+        }
+        this.newQuote = this.newQuote.bind(this);
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    newQuote() {
+
+        let getIndex = () => Math.floor(Math.random() * quotes.length);
+
+        let i = getIndex();
+
+        while (i === this.state.index) {
+            i = getIndex();
+        }
+        
+        this.setState({
+            index: i,
+            quote: quotes[i].quote,
+            author: quotes[i].author
+        });
+    }
+
+    componentDidMount() {
+        this.newQuote();
+    }
+    
+    render() {
+        return (
+            <div id="quote-box" class="container justify-content-center position-absolute top-50 start-50 translate-middle">
+                <i class="fa fa-quote-left text-secondary"></i>
+                <p id="text" class="text-center px-3 mb-0">{this.state.quote}</p>
+                <i class="fa fa-quote-right text-secondary float-end"></i>
+                <br></br>
+                <p id="author" class="text-center">- {this.state.author}</p>
+                <div class="row justify-content-between">
+                    <div class="col text-center">
+                        <button type="button" id="new-quote" class="btn btn-primary" onClick={this.newQuote}>New Quote</button>
+                    </div>
+                    <div class="col text-center">
+                        <a id="tweet-quote" class="fab fa-twitter-square" href="https://twitter.com/intent/tweet" target="_blank"></a>
+                    </div>    
+                </div>    
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/quotes.js
@@ -0,0 +1,92 @@
+const quotes = [
+    {
+        quote: "Perhaps I'm old and tired, but I think that the chances of finding out what's actually going on are so absurdly remote that the only thing to do is to say, 'Hang the sense of it,' and keep yourself busy. I'd much rather be happy than right any day.",
+        author: "Slartibartfast"
+    },
+    {
+        quote: "Empty your mind, be formless. Shapeless, like water. If you put water into a cup, it becomes the cup. You put water into a bottle and it becomes the bottle. You put it in a teapot, it becomes the teapot. Now, water can flow or it can crash. Be water, my friend.",
+        author: "Bruce Lee"
+    },
+    {
+        quote: "I am the gatekeeper of my own destiny and I will have my glory day in the hot sun.",
+        author: "Ignacio"
+    },
+    {
+        quote: "You are the night, you are the ocean. You are the light behind the cloud. You are the end and the beginning. A world where time is not allowed.",
+        author: "Daft Punk"
+    },
+    {
+        quote: "There's no such thing as competition. To find our way we lose control. Remember love's our only mission. This is a journey of the soul.",
+        author: "Daft Punk"
+    },
+    {
+        quote: "The perfect song is framed with silence. It speaks of places never seen. Your home's a promise long forgotten. It is the birthplace of your dreams.",
+        author: "Daft Punk"
+    },
+    {
+        quote: "So throw away your baggage and go forward. There are quicksands all about you, sucking at your feet, trying to suck you down into fear and self-pity and despair. That’s why you must walk so lightly. Lightly my darling, on tiptoes and no luggage, not even a sponge bag, completely unencumbered.",
+        author: "Aldous Huxley"
+    },
+    {
+        quote: "So my idea of neurotic is spending too much time trying to correct a wrong. When I feel that I’m doing that, then I snap out of it.",
+        author: "Gene Wilder"
+    },
+    {
+        quote: "Success is a terrible thing and a wonderful thing. If you can enjoy it, it's wonderful. If it starts eating away at you, and they're waiting for more from me, or what can I do to top this, then you're in trouble. Just do what you love. That's all I want to do.",
+        author: "Gene Wilder"
+    },
+    {
+        quote: "Do not try and bend the spoon, that's impossible. Instead, only try to realize the truth... There is no spoon... Then you'll see that it is not the spoon that bends, it is only yourself.",
+        author: "Spoon Boy"
+    },
+    {
+        quote: "Yes, there are two paths you can go by, but in the long run there's still time to change the road you're on.",
+        author: "Led Zeppelin"
+    },
+    {
+        quote: "When the power of love overcomes the love of power, the world will know peace.",
+        author: "Jimi Hendrix"
+    },
+    {
+        quote: "Our greatest glory is not in never falling, but in rising every time we fall.",
+        author: "Confucius"
+    },
+    {
+        quote: "It does not matter how slowly you go so long as you do not stop.",
+        author: "Confucius"
+    },
+    {
+        quote: "I've failed over and over and over again in my life. And that is why I succeed.",
+        author: "Michael Jordan"
+    },
+    {
+        quote: "The most difficult thing is the decision to act, the rest is merely tenacity.",
+        author: "Amelia Earhart"
+    },
+    {
+        quote: "If you hear a voice within you say 'you cannot paint,' then by all means paint and that voice will be silenced.",
+        author: "Vincent Van Gogh"
+    },
+    {
+        quote: "When I was five years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up. I wrote down ‘happy.’ They told me I didn’t understand the assignment, and I told them they didn’t understand life.",
+        author: "John Lennon"
+    },
+    {
+        quote: "When I let go of what I am, I become what I might be.",
+        author: "Lao Tzu"
+    },
+    {
+        quote: "The person who says it cannot be done should not interrupt the person who is doing it.",
+        author: "Chinese Proverb"
+    },
+    {
+        quote: "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.",
+        author: "Henry Ford"
+    },
+    {
+        quote: "People ask me, 'What is the use of climbing Mount Everest?' and my answer must at once be, 'It is of no use . . .' If you cannot understand that there is something in man which responds to the challenge of this mountain and goes out to meet it, that the struggle is the struggle of life itself upward and forever upward, then you won't see why we go. What we get from this adventure is just sheer joy. And joy is, after all, the end of life. We do not live to eat and make money. We eat and make money to be able to live. That is what life means and what life is for.",
+        author: "George Mallory"
+    }
+];
+
+export default quotes;
